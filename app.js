789c3e204cdc2f71bba642a89aae20b80e9c1031
Giedrius Kudelis <giedrius.kudelis@gmail.com>
2018-04-29 12:13:29 +0100

    Add jQuery and use it to bind auth function to the auth button

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-var redirect_url = "https://accounts.spotify.com/authorize";
-redirect_url += "?client_id=5c249e8331d248ca9b95ade12e7e0c4b";
-redirect_url += "&response_type=token";
-redirect_url += "&redirect_uri="+encodeURIComponent('http://virtual.gq/bpm-sort/index.html');
-redirect_url += "&state=todo";
-redirect_url += "&scope=user-read-email";
+$("#auth").click(auth)
 
 function auth() {
+    var redirect_url = "https://accounts.spotify.com/authorize";
+    redirect_url += "?client_id=5c249e8331d248ca9b95ade12e7e0c4b";
+    redirect_url += "&response_type=token";
+    redirect_url += "&redirect_uri="+encodeURIComponent('http://virtual.gq/bpm-sort/index.html');
+    redirect_url += "&state=todo";
+    redirect_url += "&scope=user-read-email";
+
     window.location.replace(redirect_url);
 }
