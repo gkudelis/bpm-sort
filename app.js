6adcf820205cfceee265e45b61c254cdf5a5b4e8
Giedrius Kudelis <giedrius.kudelis@gmail.com>
2018-05-01 22:09:33 +0100

    Get playlist details, grab names, collect into array

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 $(function() {
+    qs = new URLSearchParams(window.location.hash.substring(1));
+    if (qs.has('access_token')) {
+        var access_token = qs.get('access_token');
+
+        $("#playlists").show();
+        $("#sort").show();
+
+        get_playlists()
+            .then(get_playlist_names)
+            .then(handle_playlists);
+    } else {
+        $("#auth")
+            .click(auth)
+            .show();
+    }
+
     function auth() {
         console.log("starting auth");
 
@@ -12,21 +28,34 @@ $(function() {
         window.location.replace(redirect_url);
     }
 
-    function handle_user_profile(response) {
-        console.log(response);
+    function get_playlists() {
+        return $.ajax({
+            url: 'https://api.spotify.com/v1/me/playlists',
+            headers: { 'Authorization': 'Bearer ' + access_token },
+        });
     }
 
-    qs = new URLSearchParams(window.location.hash.substring(1));
-    if (qs.has('access_token')) {
-        var access_token = qs.get('access_token');
-        $.ajax({
-            url: 'https://api.spotify.com/v1/me',
-            headers: { 'Authorization': 'Bearer ' + access_token },
-            success: handle_user_profile
+    function get_playlist_names(playlist_data) {
+        var playlist_promises = playlist_data.items.map(function(playlist) {
+            return $.ajax({
+                url: playlist.href,
+                headers: { 'Authorization': 'Bearer ' + access_token },
+            }).then(function(playlist_details) {
+                playlist.name = playlist_details.name;
+                console.log(playlist);
+                return playlist;
+            });
         });
-    } else {
-        $("#auth")
-            .click(auth)
-            .show();
+        return wait_for_promises(playlist_promises);
     }
+
+    function handle_playlists(playlist_data) {
+        console.log(playlist_data);
+    }
+
+    function wait_for_promises(promises) {
+        return $.when.apply($, promises).then(collect_to_array);
+    }
+
+    function collect_to_array() { return Array.prototype.slice.call(arguments); }
 });
